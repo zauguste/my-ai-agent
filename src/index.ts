ff79da349277ce962c519da9cca1f583bec390f7
zauguste <zauguste52@gmail.com>
2026-09-07 11:10:49 -0400

    fix: add DELETE to CORS allowed methods

--- src/index.ts
+++ src/index.ts
@@ -254,7 +254,7 @@ export default {
       return new Response(null, {
         headers: {
           "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE",
           "Access-Control-Allow-Headers": "Content-Type, Authorization",
         },
       });
